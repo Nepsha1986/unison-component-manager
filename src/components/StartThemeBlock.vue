46f2b1379d5cc64b8b932242d75168b90ff2a89b
Alex Nepsha <alex.nepsha@4eck-media.de>
2018-03-11 03:55:12 +0200

    // Tiny fix

--- src/components/StartThemeBlock.vue
+++ src/components/StartThemeBlock.vue
@@ -2,6 +2,6 @@
     <div class="start-theme-block">
         <h2>Start theme</h2>
         <p>Start theme based on <a href="http://unyson.io/">Unyson framework</a> you can download from here </p>
-        <a class="btn btn-primary" href="https://github.com/Nepsha1986/unison-based-starter-theme/archive/master.zip">Download</a>
+        <a class="btn btn-primary" href="https://github.com/Nepsha1986/unison-based-starter-theme/archive/develop.zip">Download</a>
     </div>
 </template>
